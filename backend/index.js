acb91d7a353aecd47f31b7c401071dad0b9a639a
gks2022004 <imgauravkumar7@gmail.com>
2024-07-14 01:59:45 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -60,7 +60,7 @@ app.post("/upload", parser.single("image"), (req, res) => {
 
 // Start server
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${process.env.PORT} PORT`);
 });
 
 app.use('/stripe', stripeRoute); 
